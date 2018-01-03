1aea1c34bb4ed0fe5a79d5d90f8a85848df3332e
Glitch (my-facebook-messenger) <none>
2018-01-03 15:09:24 +0000

    🍈💇 Checkpoint

--- app.js
+++ app.js
@@ -33,13 +33,10 @@ app.post('/webhook', (req, res) => {
 
   // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
-
     // Iterate over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
-
-      // Gets the body of the webhook event
+    // Gets the body of the webhook event
     let webhook_event = entry.messaging[0];
-
     // Get the sender PSID
     let sender_psid = webhook_event.sender.id;
     console.log('Sender PSID: ' + sender_psid);
@@ -52,23 +49,18 @@ app.post('/webhook', (req, res) => {
       handlePostback(sender_psid, webhook_event.postback);
     }
     });
-
     // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
-
   } else {
     // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
-
 });
 
 // Accepts GET requests at the /webhook endpoint
 app.get('/webhook', (req, res) => {
-  
   /** UPDATE YOUR VERIFY TOKEN **/
   const VERIFY_TOKEN = "randomrandom";
-  
   // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
@@ -76,14 +68,11 @@ app.get('/webhook', (req, res) => {
     
   // Check if a token and mode were sent
   if (mode && token) {
-  
     // Check the mode and token sent are correct
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
       // Respond with 200 OK and challenge token from the request
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
-    
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);      
