7523d66195663331ccdb012a7b6a4798f0a0380d
Glitch (my-facebook-messenger) <none>
2018-01-11 15:55:18 +0000

    👽💶 Checkpoint app.js:603736/186

--- app.js
+++ app.js
@@ -40,34 +40,39 @@ app.get('/webhook', (req, res) => {
 });
 // Accepts POST requests at /webhook endpoint
 app.post('/webhook', (req, res) => {
-  let body = req.body;
-  if (body.object === 'page') {
-    
-    // Iterate over each entry - there may be multiple if batched
-    body.entry.forEach(function(pageEntry) {
-    console.log('Inside pageEntry');
-    let webhook_event = pageEntry.messaging[0];
-    // Get the sender PSID
-    let sender_psid = webhook_event.sender.id;
-    console.log('Sender PSID: ' + sender_psid);
-    console.log(webhook_event);
-    if (webhook_event.message) {
-      handleMessage(sender_psid, webhook_event.message);        
-    } else if (webhook_event.postback) {
-      handlePostback(sender_psid, webhook_event.postback);
+  var data = req.body;
+
+    // Make sure this is a page subscription
+    if (data.object == 'page') {
+        // Iterate over each entry
+        // There may be multiple if batched
+        data.entry.forEach(function (pageEntry) {
+
+            // Iterate over each messaging event
+            pageEntry.messaging.forEach(function (messagingEvent) {
+                if (messagingEvent.message) {
+                    handleMessage(messagingEvent);
+                } else if (messagingEvent.postback) {
+                    handlePostback(messagingEvent);
+                } else {
+                    console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+                }
+            });
+        });
+
+        res.sendStatus(200);
     }
-    });
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    res.sendStatus(404);
-  }
 });
 
 // Handles messages events
-function handleMessage(sender_psid, received_message) {
+function handleMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
   let response;
-  console.log(received_message.text);
-  return wit.message(received_message.text).then(({entities}) => {
+  console.log('Message text: ' + message.text);
+  return wit.message(message.text).then(({entities}) => {
     const intent = firstEntity(entities, 'intent');
     const greetings = firstEntity(entities, 'greetings');
     const location = firstEntity(entities, 'location');
@@ -89,16 +94,20 @@ function handleMessage(sender_psid, received_message) {
     } else if(greetings){
       response = { "text":"Hello"};
     }
-  callSendAPI(sender_psid, response);
+  callSendAPI(senderID, response);
   });
 }
 
 // Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-let response;
+function handlePostback(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+  let response;
   
   // Get the payload for the postback
-  let payload = received_postback.payload;
+  let payload = event.payload;
 
   // Set the response based on the postback payload
   if (payload === 'yes') {
@@ -107,7 +116,7 @@ let response;
     response = { "text": "Oops, try sending another image." }
   }
   // Send the message to acknowledge the postback
-  callSendAPI(sender_psid, response);
+  callSendAPI(senderID, response);
 }
 
 // Sends response messages to facebook via the Send API
